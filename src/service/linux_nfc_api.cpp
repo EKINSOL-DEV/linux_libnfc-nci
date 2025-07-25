83120969f33f03e9dea8044fd7d1959589e26f89
Nicky Goethals <nicky@ekinsol.be>
2025-07-25 16:53:11 +0200

    Update linux_nfc_api.cpp

--- src/service/linux_nfc_api.cpp
+++ src/service/linux_nfc_api.cpp
@@ -369,7 +369,7 @@ int nfcManager_setConfig(unsigned char id, unsigned char length, unsigned char* 
 int doWriteT4tData(unsigned char *command, unsigned char *ndef_buffer, int ndef_buffer_length)
 {
     int ret = 0;
-    if (ndef_buffer == NULL || *ndef_buffer_length <= 0) {
+    if (ndef_buffer == NULL || ndef_buffer_length == NULL || *ndef_buffer_length <= 0) {
       NXPLOG_API_E ("%s: invalide buffer!", __FUNCTION__);
       return NFA_STATUS_FAILED;
     }
